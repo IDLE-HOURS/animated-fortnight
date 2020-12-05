a29f90306ba245cd6a634ed02a8932f99a78eee1
Ash Caplan <accaplan93@gmail.com>
2020-12-05 13:27:44 +0100

    initial seed

---
+++ web/src/components/disclaimer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Disclaimer = () => {
+  return (
+    <div className='bg--blue x color--white'>
+      <div className='container--1000 mxa p2 tc outer'>
+        <div>
+          <p>Hey booger!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
